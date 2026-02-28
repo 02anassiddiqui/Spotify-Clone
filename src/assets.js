2b64633d3cb07069f1588bfee9cf103e503913c7
Mohd. Anas <anas.sd0786@gmail.com>
2026-02-28 16:45:26 +0530

    Spotify Clone

---
+++ src/assets.js
@@ -0,0 +1,55 @@
+import home_icon from "./assets/home.png";
+import search_icon from "./assets/search.png";
+import library_icon from "./assets/stack.png";
+
+import arrow_icon from "./assets/arrow.png";
+import left_arrow_icon from "./assets/left_arrow.png";
+import right_arrow_icon from "./assets/right_arrow.png";
+import plus_icon from "./assets/plus.png";
+
+import shuffle_icon from "./assets/shuffle.png";
+import play_icon from "./assets/play.png";
+import pause_icon from "./assets/pause.png";
+import next_icon from "./assets/next.png";
+import prev_icon from "./assets/prev.png";
+import loop_icon from "./assets/loop.png";
+import volume_icon from "./assets/volume.png";
+
+import mic_icon from "./assets/mic.png";
+import queue_icon from "./assets/queue.png";
+import device_icon from "./assets/speaker.png";
+import zoom_icon from "./assets/zoom.png";
+import like_icon from "./assets/like.png";
+import bell_icon from "./assets/bell.png";
+import clock_icon from "./assets/clock_icon.png";
+import mini_player_icon from "./assets/mini-player.png";
+import spotify_logo from "./assets/spotify_logo.png";   // ✅ Added
+
+export const assets = {
+  home_icon,
+  search_icon,
+  library_icon,
+
+  arrow_icon,
+  left_arrow_icon,
+  right_arrow_icon,
+  plus_icon,
+
+  shuffle_icon,
+  play_icon,
+  pause_icon,
+  next_icon,
+  prev_icon,
+  loop_icon,
+  volume_icon,
+
+  mic_icon,
+  queue_icon,
+  device_icon,
+  zoom_icon,
+  like_icon,
+  bell_icon,
+  clock_icon,
+  mini_player_icon,
+  spotify_logo, 
+};
